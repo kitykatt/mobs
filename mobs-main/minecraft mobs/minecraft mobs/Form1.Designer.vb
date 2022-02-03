4381e9ec717d37a4943192532984fd3506a1295a
kitykatt <90800597+kitykatt@users.noreply.github.com>
2022-02-02 22:25:52 -0700

    Add files via upload

---
+++ mobs-main/minecraft mobs/minecraft mobs/Form1.Designer.vb
@@ -0,0 +1,341 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form9001
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.last_button = New System.Windows.Forms.Button()
+        Me.first_button = New System.Windows.Forms.Button()
+        Me.Prev_button = New System.Windows.Forms.Button()
+        Me.next_button = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.health = New System.Windows.Forms.Label()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.damage = New System.Windows.Forms.Label()
+        Me.effect = New System.Windows.Forms.Label()
+        Me.PictureBox10 = New System.Windows.Forms.PictureBox()
+        Me.name1 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.Image = Global.minecraft_mobs.My.Resources.Resources.Zombie
+        Me.PictureBox8.Location = New System.Drawing.Point(128, 2)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(250, 198)
+        Me.PictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox8.TabIndex = 7
+        Me.PictureBox8.TabStop = False
+        Me.PictureBox8.Visible = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.Image = Global.minecraft_mobs.My.Resources.Resources.Wither_Skeleton
+        Me.PictureBox7.Location = New System.Drawing.Point(128, 2)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(250, 198)
+        Me.PictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox7.TabIndex = 6
+        Me.PictureBox7.TabStop = False
+        Me.PictureBox7.Visible = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = Global.minecraft_mobs.My.Resources.Resources.Shulker
+        Me.PictureBox6.Location = New System.Drawing.Point(128, 2)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(250, 198)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox6.TabIndex = 5
+        Me.PictureBox6.TabStop = False
+        Me.PictureBox6.Visible = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = Global.minecraft_mobs.My.Resources.Resources.phantom
+        Me.PictureBox5.Location = New System.Drawing.Point(128, 2)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(250, 198)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 4
+        Me.PictureBox5.TabStop = False
+        Me.PictureBox5.Visible = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.minecraft_mobs.My.Resources.Resources.Panda
+        Me.PictureBox4.Location = New System.Drawing.Point(128, 2)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(250, 198)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 3
+        Me.PictureBox4.TabStop = False
+        Me.PictureBox4.Visible = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.minecraft_mobs.My.Resources.Resources.Iron_Golem
+        Me.PictureBox3.Location = New System.Drawing.Point(128, 2)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(250, 198)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        Me.PictureBox3.Visible = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.minecraft_mobs.My.Resources.Resources.creeper
+        Me.PictureBox2.Location = New System.Drawing.Point(128, 2)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(250, 198)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.minecraft_mobs.My.Resources.Resources.Enderman
+        Me.PictureBox1.Location = New System.Drawing.Point(128, 2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(250, 198)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        '
+        'last_button
+        '
+        Me.last_button.Location = New System.Drawing.Point(38, 177)
+        Me.last_button.Name = "last_button"
+        Me.last_button.Size = New System.Drawing.Size(75, 23)
+        Me.last_button.TabIndex = 25
+        Me.last_button.Text = "last"
+        Me.last_button.UseVisualStyleBackColor = True
+        '
+        'first_button
+        '
+        Me.first_button.Location = New System.Drawing.Point(38, 148)
+        Me.first_button.Name = "first_button"
+        Me.first_button.Size = New System.Drawing.Size(75, 23)
+        Me.first_button.TabIndex = 24
+        Me.first_button.Text = "first"
+        Me.first_button.UseVisualStyleBackColor = True
+        '
+        'Prev_button
+        '
+        Me.Prev_button.Location = New System.Drawing.Point(38, 119)
+        Me.Prev_button.Name = "Prev_button"
+        Me.Prev_button.Size = New System.Drawing.Size(75, 23)
+        Me.Prev_button.TabIndex = 23
+        Me.Prev_button.Text = "prev"
+        Me.Prev_button.UseVisualStyleBackColor = True
+        '
+        'next_button
+        '
+        Me.next_button.Location = New System.Drawing.Point(38, 90)
+        Me.next_button.Name = "next_button"
+        Me.next_button.Size = New System.Drawing.Size(75, 23)
+        Me.next_button.TabIndex = 22
+        Me.next_button.Text = "next"
+        Me.next_button.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(1, 52)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(50, 13)
+        Me.Label5.TabIndex = 32
+        Me.Label5.Text = "Damage:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(8, 30)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 13)
+        Me.Label4.TabIndex = 30
+        Me.Label4.Text = "Health:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(11, 74)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(38, 13)
+        Me.Label6.TabIndex = 34
+        Me.Label6.Text = "Effect:"
+        '
+        'health
+        '
+        Me.health.AutoSize = True
+        Me.health.Location = New System.Drawing.Point(57, 30)
+        Me.health.Name = "health"
+        Me.health.Size = New System.Drawing.Size(19, 13)
+        Me.health.TabIndex = 35
+        Me.health.Text = "40"
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.Image = Global.minecraft_mobs.My.Resources.Resources.mcbee
+        Me.PictureBox9.Location = New System.Drawing.Point(128, 2)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(250, 198)
+        Me.PictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox9.TabIndex = 36
+        Me.PictureBox9.TabStop = False
+        Me.PictureBox9.Visible = False
+        '
+        'damage
+        '
+        Me.damage.AutoSize = True
+        Me.damage.Location = New System.Drawing.Point(57, 52)
+        Me.damage.Name = "damage"
+        Me.damage.Size = New System.Drawing.Size(13, 13)
+        Me.damage.TabIndex = 37
+        Me.damage.Text = "7"
+        '
+        'effect
+        '
+        Me.effect.AutoSize = True
+        Me.effect.Location = New System.Drawing.Point(57, 74)
+        Me.effect.Name = "effect"
+        Me.effect.Size = New System.Drawing.Size(31, 13)
+        Me.effect.TabIndex = 38
+        Me.effect.Text = "none"
+        '
+        'PictureBox10
+        '
+        Me.PictureBox10.Image = Global.minecraft_mobs.My.Resources.Resources.skeleton
+        Me.PictureBox10.Location = New System.Drawing.Point(128, 2)
+        Me.PictureBox10.Name = "PictureBox10"
+        Me.PictureBox10.Size = New System.Drawing.Size(250, 198)
+        Me.PictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox10.TabIndex = 39
+        Me.PictureBox10.TabStop = False
+        Me.PictureBox10.Visible = False
+        '
+        'name1
+        '
+        Me.name1.AutoSize = True
+        Me.name1.Location = New System.Drawing.Point(49, 9)
+        Me.name1.Name = "name1"
+        Me.name1.Size = New System.Drawing.Size(54, 13)
+        Me.name1.TabIndex = 40
+        Me.name1.Text = "enderman"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(1, 9)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(36, 13)
+        Me.Label8.TabIndex = 41
+        Me.Label8.Text = "name:"
+        '
+        'Form9001
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(759, 516)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.name1)
+        Me.Controls.Add(Me.effect)
+        Me.Controls.Add(Me.damage)
+        Me.Controls.Add(Me.health)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.last_button)
+        Me.Controls.Add(Me.first_button)
+        Me.Controls.Add(Me.Prev_button)
+        Me.Controls.Add(Me.next_button)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox10)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Name = "Form9001"
+        Me.Text = "Form1"
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents last_button As Button
+    Friend WithEvents first_button As Button
+    Friend WithEvents Prev_button As Button
+    Friend WithEvents next_button As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents health As Label
+    Friend WithEvents PictureBox9 As PictureBox
+    Friend WithEvents damage As Label
+    Friend WithEvents effect As Label
+    Friend WithEvents PictureBox10 As PictureBox
+    Friend WithEvents name1 As Label
+    Friend WithEvents Label8 As Label
+End Class
---
+++ mobs-main/minecraft mobs/minecraft mobs/Form1.vb
@@ -0,0 +1,110 @@
+﻿Public Class Form9001
+    Dim pic As Integer = 1
+    Function picture() As Boolean
+        PictureBox1.Visible = False
+        PictureBox2.Visible = False
+        PictureBox3.Visible = False
+        PictureBox4.Visible = False
+        PictureBox5.Visible = False
+        PictureBox6.Visible = False
+        PictureBox7.Visible = False
+        PictureBox8.Visible = False
+        PictureBox9.Visible = False
+        PictureBox10.Visible = False
+        If pic = 1 Then
+            PictureBox1.Visible = True
+            health.Text = 40
+            damage.Text = 7
+            effect.Text = "none"
+            name1.Text = "enderman"
+        End If
+        If pic = 2 Then
+            PictureBox2.Visible = True
+            health.Text = 20
+            damage.Text = 22
+            effect.Text = "none"
+            name1.Text = "creeper"
+        End If
+        If pic = 3 Then
+            PictureBox3.Visible = True
+            health.Text = 100
+            damage.Text = 10
+            effect.Text = "none"
+            name1.Text = "iron golem"
+        End If
+        If pic = 4 Then
+            PictureBox4.Visible = True
+            health.Text = 20
+            damage.Text = 6
+            effect.Text = "none"
+            name1.Text = "panda"
+        End If
+        If pic = 5 Then
+            PictureBox5.Visible = True
+            health.Text = 20
+            damage.Text = 2
+            effect.Text = "none"
+            name1.Text = "phantom"
+        End If
+        If pic = 6 Then
+            PictureBox6.Visible = True
+            health.Text = 30
+            damage.Text = 4
+            effect.Text = "levitation"
+            name1.Text = "shulker"
+        End If
+        If pic = 7 Then
+            PictureBox7.Visible = True
+            health.Text = 20
+            damage.Text = 8
+            effect.Text = "wither"
+            name1.Text = "wither skeleton"
+        End If
+        If pic = 8 Then
+            PictureBox8.Visible = True
+            health.Text = 20
+            damage.Text = 3
+            effect.Text = "none"
+            name1.Text = "zombie"
+        End If
+        If pic = 9 Then
+            PictureBox9.Visible = True
+            health.Text = 10
+            damage.Text = 2
+            effect.Text = "poison"
+            name1.Text = "bee"
+        End If
+        If pic = 10 Then
+            PictureBox10.Visible = True
+            health.Text = 20
+            damage.Text = 4
+            effect.Text = "none"
+            name1.Text = "skeleton"
+        End If
+    End Function
+    Private Sub next_button_Click(sender As Object, e As EventArgs) Handles next_button.Click
+        pic = pic + 1
+        If pic = 11 Then
+            pic = 1
+        End If
+        picture()
+    End Sub
+
+    Private Sub Prev_button_Click(sender As Object, e As EventArgs) Handles Prev_button.Click
+        pic = pic - 1
+        If pic = 0 Then
+            pic = 10
+        End If
+        picture()
+    End Sub
+
+    Private Sub first_button_Click(sender As Object, e As EventArgs) Handles first_button.Click
+        pic = 1
+        picture()
+    End Sub
+
+    Private Sub last_button_Click(sender As Object, e As EventArgs) Handles last_button.Click
+        pic = 10
+        picture()
+    End Sub
+End Class
